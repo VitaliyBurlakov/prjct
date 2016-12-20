22909d71866b8b474affaed622e8a06b579f1e80
nmbrone <sergey.snozyk@gmail.com>
2016-12-20 22:44:36 +0200

    upd

--- src/js/routes/signup.js
+++ src/js/routes/signup.js
@@ -1,32 +1,77 @@
-function signup() {
-  // createUserWithEmailAndPassword
+function signup(ctx, next) {
+  if (ctx.user) {
+    return page.redirect('/profile');
+  }
+
   render('signup');
 
-  let form = document.getElementById('signup-form');
+  const signupForm      = document.forms['signup-form'];
+  const errorsContainer = qs('#errors', signupForm);
+  const auth            = firebase.auth();
 
-  let {email, password, password_confirm} = form;
+  function renderErrors(errors = []) {
+    return errors.map(err => {
+      return `
+        <li class="list-group-item list-group-item-danger">
+          <span>${err}</span>
+        </li>
+      `;
+    }).join('');
+  }
 
-  form.addEventListener('submit', submitHandler);
+  function showErrors(errors = []) {
+    errorsContainer.innerHTML = renderErrors(errors);
+    errorsContainer.hidden = false;
+  }
 
-  // 1. Нету обработки серверных ошибок
-  // 2. Нету обработки случая когда пользователь был успешно создан
-  // 3. Нету информативности при клиентской валидации
-  function submitHandler(e) {
+  function hideErrors() {
+    errorsContainer.hidden = true;
+    errorsContainer.innerHTML = '';
+  }
 
-    // 1. @
-    // 2. .
-    // 3. pass.length > 6
-    // 4. pass === pass_confirm
+  function onUserCreated(user) {
+    // const usersRef = firebase.database().ref(`users/${user.uid}`);
+    // const userData = pick(user, ['uid', 'email', 'displayName', 'photoURL']);
+    // usersRef.set(userData)
+    //   .then(() => {
+    //     user.sendEmailVerification();
+    //     page('/profile');
+    //   });
+    console.log('Success');
+  }
 
-    if ((email.value.indexOf('@') === -1) || (email.value.indexOf('.') === -1) || (password.value.length < 6) || (password.value !== password_confirm.value)) {
-      alert('Error');
-    } else {
-      firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-        .catch((error) => {
-          alert(error.message);
-        });
+  function onUserCreationError(error) {
+    // unsetLoadingState();
+    showError(error.message);
+  }
+
+  function handler(e) {
+    const form = e.target;
+    const { email, password, password_confirm } = form.elements;
+    const errors = [];
+
+    if (email.value.indexOf('@') === -1) {
+      errors.push('Email is invalid');
     }
+
+    if (!password.value.length) {
+      errors.push('Please enter password');
+    } else if (password.value !== password_confirm.value) {
+      errors.push('Password is incorrect');
+    }
+
+    if (errors.length) {
+      return showErrors(errors);
+    }
+
+    hideErrors();
+    auth
+      .createUserWithEmailAndPassword(email.value, password.value)
+      .then(onUserCreated)
+      .catch(onUserCreationError);
 
     e.preventDefault();
   }
+
+  signupForm.addEventListener('submit', handler);
 }
