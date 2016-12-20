e686ad0e52119989f7ba921062d77d182f6ade27
nmbrone <sergey.snozyk@gmail.com>
2016-12-20 20:31:08 +0200

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,46 @@
-console.log('Hello world!!!');
-console.log(
-  templates.main({
-    name: 'Jhon'
-  })
-);
+'use strict';
+
+(function() {
+
+  firebase.initializeApp({
+    apiKey: 'AIzaSyDdklaao0vq0DcAkCXzYFoZeVJTy9kOoGA',
+    authDomain: 'prjctr-84d57.firebaseapp.com',
+    databaseURL: 'https://prjctr-84d57.firebaseio.com',
+    storageBucket: 'prjctr-84d57.appspot.com',
+    messagingSenderId: '459426391837'
+  });
+
+  //=require 'lib/*.js'
+  //=require 'middlewares/*.js'
+  //=require 'routes/*.js'
+
+  const { location, history, templates } = window;
+  const rootElement = qs('#root');
+
+  function render(tplName, data = {}) {
+    const user = firebase.auth().currentUser;
+    const userData = user ? user.toJSON() : null;
+    data = Object.assign(data, { user: userData });
+    rootElement.innerHTML = templates[tplName](data);
+  }
+
+  function render404() {
+    render('404');
+  }
+
+  page('*', auth);
+  page('/', main);
+  page('/login', login);
+  page('/logout', logout);
+  page('/signup', signup);
+  page('*', render404);
+
+  render('preloader');
+
+  // simulate firebase 'onready' behavior
+  const unsubsribe = firebase.auth().onAuthStateChanged(() => {
+    page();
+    unsubsribe();
+  });
+
+} ());
