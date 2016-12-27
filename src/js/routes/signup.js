9d6ef49ac1728196234447ad212700e6b8a6a010
nmbrone <sergey.snozyk@gmail.com>
2016-12-27 15:03:15 +0200

    update signup route

--- src/js/routes/signup.js
+++ src/js/routes/signup.js
@@ -5,12 +5,13 @@ function signup(ctx, next) {
 
   render('signup');
 
-  const signupForm      = document.forms['signup-form'];
-  const errorsContainer = qs('#errors', signupForm);
   const auth            = firebase.auth();
+  const signupForm      = document.forms['signup-form'];
+  const submitBtn       = qs('[type="submit"]', signupForm);
+  const errorsContainer = qs('#errors', signupForm);
 
-  function renderErrors(errors = []) {
-    return errors.map(err => {
+  function renderError(errors = []) {
+    return [].concat(errors).map(err => {
       return `
         <li class="list-group-item list-group-item-danger">
           <span>${err}</span>
@@ -19,58 +20,72 @@ function signup(ctx, next) {
     }).join('');
   }
 
-  function showErrors(errors = []) {
-    errorsContainer.innerHTML = renderErrors(errors);
+  function showErrors(errors) {
+    // render all error messages
+    errorsContainer.innerHTML = renderError(errors);
+    // show error container
     errorsContainer.hidden = false;
   }
 
-  function hideErrors() {
-    errorsContainer.hidden = true;
+  function hideErrors(errorName) {
     errorsContainer.innerHTML = '';
+    errorsContainer.hidden = true;
+  }
+
+  function setLoadingState() {
+    signupForm.classList.add('is-loading');
+    submitBtn.setAttribute('disabled', true);
+  }
+
+  function unsetLoadingState() {
+    signupForm.classList.remove('is-loading');
+    submitBtn.removeAttribute('disabled');
   }
 
   function onUserCreated(user) {
-    // const usersRef = firebase.database().ref(`users/${user.uid}`);
-    // const userData = pick(user, ['uid', 'email', 'displayName', 'photoURL']);
-    // usersRef.set(userData)
-    //   .then(() => {
-    //     user.sendEmailVerification();
-    //     page('/profile');
-    //   });
-    console.log('Success');
+    const usersRef = firebase.database().ref(`users/${user.uid}`);
+    const userData = pick(user, ['uid', 'email', 'displayName', 'photoURL']);
+    usersRef.set(userData)
+      .then(() => {
+        user.sendEmailVerification();
+        page.redirect('/profile');
+      });
   }
 
   function onUserCreationError(error) {
-    // unsetLoadingState();
-    showError(error.message);
+    unsetLoadingState();
+    showErrors(error.message);
   }
 
   function handler(e) {
-    const form = e.target;
-    const { email, password, password_confirm } = form.elements;
     const errors = [];
+    const form   = e.target;
+    const { email, password, password_confirm } = form.elements;
+
+    e.preventDefault();
 
     if (email.value.indexOf('@') === -1) {
       errors.push('Email is invalid');
     }
 
-    if (!password.value.length) {
-      errors.push('Please enter password');
-    } else if (password.value !== password_confirm.value) {
-      errors.push('Password is incorrect');
+    if (password.value.length < 6) {
+      errors.push('Password must be at least 6 characters');
+    }
+
+    if (password.value !== password_confirm.value) {
+      errors.push('Wrong password');
     }
 
     if (errors.length) {
       return showErrors(errors);
     }
 
+    setLoadingState();
     hideErrors();
     auth
       .createUserWithEmailAndPassword(email.value, password.value)
       .then(onUserCreated)
       .catch(onUserCreationError);
-
-    e.preventDefault();
   }
 
   signupForm.addEventListener('submit', handler);
